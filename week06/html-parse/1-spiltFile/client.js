cfd6c2c4a5fc7a88595f0fbc9ea42cc41ca2a89b
xingkai17 <406710312@qq.com>
2020-06-01 22:32:48 +0800

    fix: toString

--- week06/html-parse/1-spiltFile/client.js
+++ week06/html-parse/1-spiltFile/client.js
@@ -27,7 +27,7 @@ class Request {
   }
 
   toString() {
-    return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\n${this.bodyText}`;
+    return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\n\r\n${this.bodyText}`;
   }
 
   send(connection) {
--- week06/html-parse/2-initFSM/parser.js
+++ week06/html-parse/2-initFSM/parser.js
@@ -8,6 +8,7 @@ function data(c) {
 
 module.exports.parseHTML = function parseHTML(html) {
   let state = data;
+  console.log(`state = `, html);
   for(let c of html) {
     state = state(c);
   }
